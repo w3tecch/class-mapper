08b96c69ae8b7dd8534fe10cc0c7a708005eb85d
Robert Kleger <robert.kleger@swisscom.com>
2018-05-07 09:52:46 +0200

    🐛 reverse

--- demo/demo.ts
+++
@@ -1,9 +0,0 @@
-import { MapTo } from '../dist/class-mapper';
-
-export class UglyUser {
-  @MapTo('firstName')
-  public name1: string = 'John';
-
-  @MapTo('lastName')
-  public name3: string = 'Doe';
-}
