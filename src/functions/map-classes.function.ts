5db353f582ccf95b8c2ac0cb7ca1afaf4e889e7f
Robert Kleger <robert.kleger@swisscom.com>
2018-05-10 20:20:11 +0200

    📝 add a comment to mapClasses function

--- src/functions/map-classes.function.ts
+++ src/functions/map-classes.function.ts
@@ -6,6 +6,12 @@ export interface IMapClassOptions {
   groups?: string[];
 }
 
+/**
+ * Maps your source class in your target class
+ * @param sourceClass Source model
+ * @param targetClass Target model type
+ * @param options Mapping options
+ */
 export const mapClasses = <T, U>(sourceClass: T, targetClass: IMapClass<U>, options?: IMapOptions): U => {
   const classMapper = new ClassMapper<T, U>(sourceClass, targetClass, options);
 
