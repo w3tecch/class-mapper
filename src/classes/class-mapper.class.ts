7bef71c61a52b0a39394b0a4e73167a61ddf94c5
Robert Kleger <robert.kleger@swisscom.com>
2018-10-25 14:41:11 +0200

    ✨ filter property type decorator by groups

--- src/classes/class-mapper.class.ts
+++ src/classes/class-mapper.class.ts
@@ -1,3 +1,4 @@
+import find from 'lodash-es/find';
 import { IMapClass } from '../interfaces/map-class.interface';
 import { IMapOptions } from '../interfaces/map-options.interface';
 import { IPropertyTypeOptions } from '../interfaces/property-type-options.interface';
@@ -22,7 +23,10 @@ export class ClassMapper<T, U> {
     const mapFromSourceMetadata = this.filterMetadata();
 
     mapFromSourceMetadata.forEach(metadata => {
-      const propertyTypeMetadata = metadataStorage.getPropertyType(this.targetClass, metadata.propertyKey);
+      const propertyTypeMetadata = find(metadataStorage.getPropertyTypes(
+        this.targetClass, metadata.propertyKey),
+        t => this.checkGroups(t.options)
+      );
 
       if (propertyTypeMetadata) {
         this.assignRecursive(metadata, propertyTypeMetadata);
