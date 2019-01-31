da6f9719dc3c26f50df6368e15a8d25b2a73d477
Robert Kleger <robert.kleger@dormakaba.com>
2019-01-31 13:30:00 +0100

    💚 fix ci build

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
-import typescript from 'rollup-plugin-typescript2'
+import typescript from 'rollup-plugin-typescript2';
 import nodeResolve from 'rollup-plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
-import pkg from './package.json'
+import pkg from './package.json';
 
 export default {
   input: 'src/index.ts',
